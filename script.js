10324b00344c6363953056fddad1013aa50eed46
Morgan C <morgancuiuwu@gmail.com>
2024-10-03 20:22:02 -0400

    fixed horizontal mobile screen, work on verticle screen later

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let text = document.getElementById("ParallaxText1");
+let body = document.getElementsByTagName('body')[0];
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     text.style.marginTop = value * 1.35 + 'px';
+    // Find amount of scrollTop, then change the text when it scrolls past the second dark block
+    if(body.scrollTop > 1){
+        text.textContent = "Explore new worlds like you haven't before.";
+        console.log("scroll works");
+    }
 //     text.animate([{
 //         transform: "scale(35)",
 //         opacity: 0.25,
