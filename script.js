f1c2686bca49db687eecfe9e0293379e4d6d3eeb
Morgan C <morgancuiuwu@gmail.com>
2024-10-12 01:10:46 -0400

    added social links on bottom

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 let text = document.getElementById("ParallaxText1");
+let image1 = document.getElementById("testimg");
+let image2 = document.getElementById("testimg2");
 let body = document.getElementsByTagName('body')[0];
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
-    text.style.marginTop = value * 1.15 + 'px';
+    text.style.marginTop = value * 1.25 + 'px';
     window.onscroll = function(){myFunction()};
     // Find amount of scrollTop, then change the text when it scrolls past the second dark block
     function myFunction(){
         if(document.documentElement.scrollTop > 850){
             text.innerText = "Explore worlds like you've never before.";
+            image2.animate([{
+                transform: "scale(1.5)",
+                opacity: 0.25,
+            },
+            {
+                transform: "scale(1)",
+                opacity: 1,
+            }],{
+                duration: 1500, fill: "forwards", easing: "ease"
+            })
         }
         else{
             text.innerText = "Step into a vast new world of adventures.";
+            // image1.animate([{
+            //     transform: "scale(2)",
+            //     opacity: 0.25,
+            // },
+            // {
+            //     transform: "scale(1)",
+            //     opacity: 1,
+            // }],{
+            //     duration: 1500, fill: "forwards", easing: "ease"
+            // })
         }
     }
-//     text.animate([{
-//         transform: "scale(35)",
-//         opacity: 0.25,
-//     },
-//     {
-//         transform: "scale(2)",
-//         opacity: 1,
-//     }],{
-//         duration: 300, fill: "forwards", easing: "ease"
-//     })
 });
 
 // Make scroll animation when DOMcontent loads maybe??
