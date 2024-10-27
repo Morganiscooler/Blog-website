8b627154d1569095d0a1b1e62b063cc5c5f268cc
Morgan C <morgancuiuwu@gmail.com>
2024-10-26 23:26:59 -0400

    slideshow changes + menu added

--- script.js
+++ script.js
@@ -1,42 +1,28 @@
-let text = document.getElementById("ParallaxText1");
-let image1 = document.getElementById("testimg");
-let image2 = document.getElementById("testimg2");
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// Images will pop up when they are loaded in upon scroll
-// window.addEventListener('scroll', () => {
-//     let value = window.scrollY;
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-//     text.style.marginTop = value * 1.25 + 'px';
-//     window.onscroll = function(){myFunction()};
-//     // Find amount of scrollTop, then change the text when it scrolls past the second dark block
-//     function myFunction(){
-//         if(document.documentElement.scrollTop > 850){
-//             // text.innerText = "Explore worlds like you've never before.";
-//             // image2.animate([{
-//             //     transform: "scale(1.5)",
-//             //     opacity: 0.25,
-//             // },
-//             // {
-//             //     transform: "scale(1)",
-//             //     opacity: 1,
-//             // }],{
-//             //     duration: 1500, fill: "forwards", easing: "ease"
-//             // })
-//         }
-//         else{
-//             // text.innerText = "Step into a vast new world of adventures.";
-//             // image1.animate([{
-//             //     transform: "scale(2)",
-//             //     opacity: 0.25,
-//             // },
-//             // {
-//             //     transform: "scale(1)",
-//             //     opacity: 1,
-//             // }],{
-//             //     duration: 1500, fill: "forwards", easing: "ease"
-//             // })
-//         }
-//     }
-// });
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-// Make scroll animation when DOMcontent loads maybe??
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("Container");
+//   let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+  slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+}
