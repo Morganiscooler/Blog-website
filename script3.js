7a55bbc3fddce8afa16f5c5acabd543a80d26590
Morgan C <morgancuiuwu@gmail.com>
2024-11-09 06:59:08 -0500

    each slideshow now has its own multiple slides for displaying images

--- script3.js
+++ script3.js
@@ -1,25 +1,25 @@
-let nestSlideIndex = 1;
+let nestSlideIndex2 = 1;
 // "displaySlides(nestSlideIndex);" <-- this tells the website to display a slide by 
 // default when you load, which is good for your outer slideshow but not for your 
 // inner one 
 
 // Thumbnail image controls for inner slideshow
-function Slidedisplayed(n) {
-   displaySlides(nestSlideIndex = n);
+function Slidedisplayed2(n) {
+   displaySlides2(nestSlideIndex2 = n);
 }
 
-function displaySlides(n) {
+function displaySlides2(n) {
    let i;
 
    // Changed the class names for the inner slideshow to further ensure no entanglement with the outer slideshow code
-   let slides = document.getElementsByClassName("mySlides"); 
-   let dots = document.getElementsByClassName("dot");
+   let slides = document.getElementsByClassName("mySlides2"); 
+   let dots = document.getElementsByClassName("dot2");
 
    // If the next button is pressed on the last slide, it sets the nestSlideIndex back to 
    // 1 to show the first slide again
-   if (n > slides.length) {nestSlideIndex = 1}
+   if (n > slides.length) {nestSlideIndex2 = 1}
   // Sets to last slide index if previous button is clicked on first slide
-   if (n < 1) {nestSlideIndex = slides.length}
+   if (n < 1) {nestSlideIndex2 = slides.length}
 
    // A for-loop that makes all slide content disappear/invisible
    for (i = 0; i < slides.length; i++) {
@@ -29,6 +29,6 @@ function displaySlides(n) {
    for (i = 0; i < dots.length; i++) {
      dots[i].className = dots[i].className.replace(" active", "");
    }
-   slides[nestSlideIndex-1].style.display = "block";
-   dots[nestSlideIndex-1].className += " active";
+   slides[nestSlideIndex2-1].style.display = "block";
+   dots[nestSlideIndex2-1].className += " active";
 }
--- script4.js
+++ script4.js
@@ -1,25 +1,25 @@
-let nestSlideIndex = 1;
+let nestSlideIndex3 = 1;
 // "displaySlides(nestSlideIndex);" <-- this tells the website to display a slide by 
 // default when you load, which is good for your outer slideshow but not for your 
 // inner one 
 
 // Thumbnail image controls for inner slideshow
-function Slidedisplayed(n) {
-   displaySlides(nestSlideIndex = n);
+function Slidedisplayed3(n) {
+   displaySlides3(nestSlideIndex3 = n);
 }
 
-function displaySlides(n) {
+function displaySlides3(n) {
    let i;
 
    // Changed the class names for the inner slideshow to further ensure no entanglement with the outer slideshow code
-   let slides = document.getElementsByClassName("mySlides"); 
-   let dots = document.getElementsByClassName("dot");
+   let slides = document.getElementsByClassName("mySlides3"); 
+   let dots = document.getElementsByClassName("dot3");
 
    // If the next button is pressed on the last slide, it sets the nestSlideIndex back to 
    // 1 to show the first slide again
-   if (n > slides.length) {nestSlideIndex = 1}
+   if (n > slides.length) {nestSlideIndex3 = 1}
   // Sets to last slide index if previous button is clicked on first slide
-   if (n < 1) {nestSlideIndex = slides.length}
+   if (n < 1) {nestSlideIndex3 = slides.length}
 
    // A for-loop that makes all slide content disappear/invisible
    for (i = 0; i < slides.length; i++) {
@@ -29,6 +29,6 @@ function displaySlides(n) {
    for (i = 0; i < dots.length; i++) {
      dots[i].className = dots[i].className.replace(" active", "");
    }
-   slides[nestSlideIndex-1].style.display = "block";
-   dots[nestSlideIndex-1].className += " active";
+   slides[nestSlideIndex3-1].style.display = "block";
+   dots[nestSlideIndex3-1].className += " active";
 }
