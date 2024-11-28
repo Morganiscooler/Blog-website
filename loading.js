ec987b7baee6a6df78d569d56e35a8217746e282
Morgan C <morgancuiuwu@gmail.com>
2024-11-28 16:07:18 -0500

    submission form works for all pages

---
+++ loading.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", function(){
+    const load = document.querySelector(".loading");
+    load.className += " hidden"; // Adds the "hidden" class when loading occurs
+})
