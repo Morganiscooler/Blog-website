16a95cab8e762cf243e0b4484ba111cf059fabd6
Morgan C <morgancuiuwu@gmail.com>
2025-04-02 18:47:36 -0400

    added luke's script, bugged when adding more games by existing dev

--- search.js
+++ search.js
@@ -1,13 +1,3 @@
-
-const clearInput = () => {
-    const input = document.getElementById("clearBtn")[0];
-        input.value = "";
-        automaton.style.display = "block";
-        platformer.style.display = "block";
-
-}
-
-// copy this and make a second script for Finn, remember to modify the vars so they don't overlap.
 let searchBar = document.getElementById("searchBar");
 const platformer = document.querySelector(".Game2");
 const automaton = document.querySelector(".Game1")
@@ -16,38 +6,47 @@ searchBar.addEventListener('input', function(event){
     const searchItem = event.target.value;
     console.log(`Your search term is: ${searchItem}`);
 
-    function countLetters(searchItem, letters) {
+    // Added map for game devs and corresponding html element classes
+    // *Make sure this works for multiple games with the same devs
+    // *Add more games by Lawrence later
+    const gameDevMap = new Map();
+    gameDevMap.set("bloxy996", [".Game1"]);
+    gameDevMap.set("0ddsh33p", ".Game2");
+    
+    function countLetters(searchItem, gameList) {
         let count = 0;
         const lastIndex = searchItem.length - 1;
-        for (let i = 0; i < searchItem.length; i++) {
-          let char = searchItem[i].toLowerCase();
-        //   console.log(`the string in lowercase is ${char}`);
-        //  works! all letters in the array below are lowercase
-          if (char == letters[i]) {
-            count++; 
-            automaton.style.display = "block";
-            platformer.style.display = "none";
-            // display Lawrence's games
-          }
-          else if(char[lastIndex] !== letters[i]){
-            count--;
-            automaton.style.display = "none";
-            platformer.style.display = "none";
-          }
-          else {
-            count--; 
-            automaton.style.display = "none";
-            platformer.style.display = "none";
-            // display neither's games, eventually add text that says "No games found."
-          }
+        // new stuff using map
+        for (const [name, value] of gameList.entries()) {
+            console.log(name);
+            let game = document.querySelector(value);
+            for (let i = 0; i < searchItem.length; i++) {
+                  let char = searchItem[i].toLowerCase();
+                //   console.log(`the string in lowercase is ${char}`);
+                //  works! all letters in the array below are lowercase
+                  if (char == name[i]) {
+                      console.log(char.toString() + " is equal to " + name[i]);
+                    game.style.display = "block";
+                    count++; 
+                    // display Lawrence's games
+                  }
+                  else if(char[lastIndex] !== name[i]){
+                    game.style.display = "none";
+                    count--;
+                  }
+                  else {
+                    game.style.display = "none";
+                    count--; 
+                    // display neither's games, eventually add text that says "No games found."
+                  }
+                }
         }
         return count;
       }
-      const myString = searchItem;
-      const lettersToFind = ["b", "l", "o", "x", "y", "9", "9", "6"];
-      const occurrences = countLetters(myString, lettersToFind);
+      const myString = searchItem;    
+      const occurrences = countLetters(myString, gameDevMap);
       console.log(occurrences); // Output: 8
-      if (searchBar.value == 0) {
+      if (searchBar.value == 0 && searchItem[0] !== "0") {
         // The search input is empty
         automaton.style.display = "block";
         platformer.style.display = "block";
