f6d0bb9ef5d3dafd32d08afed9a5e5e712e89dda
Morgan C <morgancuiuwu@gmail.com>
2025-04-02 20:24:22 -0400

    SEARCH FEATURE WORKING FOR MULTIPLE DEVS NOW

--- search.js
+++ search.js
@@ -7,20 +7,23 @@ searchBar.addEventListener('input', function(event){
     console.log(`Your search term is: ${searchItem}`);
 
     // Added map for game devs and corresponding html element classes
-    // *Make sure this works for multiple games with the same devs
-    // *Add more games by Lawrence later
     const gameDevMap = new Map();
-    gameDevMap.set("bloxy996", [".Game1"]);
-    gameDevMap.set("0ddsh33p", ".Game2");
-    
+    gameDevMap.set(".Game1", "bloxy996");
+    gameDevMap.set(".Game2", "0ddsh33p");
+    gameDevMap.set(".Game3", "bloxy996");
+    gameDevMap.set(".Game4", "bloxy996");
+    gameDevMap.set(".Game5", "bloxy996");
+
     function countLetters(searchItem, gameList) {
+        console.log("The string is " + searchItem);
+      
         let count = 0;
         const lastIndex = searchItem.length - 1;
         // new stuff using map
-        for (const [name, value] of gameList.entries()) {
-            console.log(name);
-            let game = document.querySelector(value);
+        for (const [id, name] of gameList.entries()) {
+            let game = document.querySelector(id);
             for (let i = 0; i < searchItem.length; i++) {
+                  //console.log(i);
                   let char = searchItem[i].toLowerCase();
                 //   console.log(`the string in lowercase is ${char}`);
                 //  works! all letters in the array below are lowercase
@@ -28,28 +31,35 @@ searchBar.addEventListener('input', function(event){
                       console.log(char.toString() + " is equal to " + name[i]);
                     game.style.display = "block";
                     count++; 
+                    //automaton.style.display = "block";
+                    //platformer.style.display = "none";
                     // display Lawrence's games
                   }
                   else if(char[lastIndex] !== name[i]){
                     game.style.display = "none";
                     count--;
+                    //automaton.style.display = "none";
+                    //platformer.style.display = "none";
                   }
                   else {
+                    console.log("yeah nothing");
                     game.style.display = "none";
                     count--; 
+                    //automaton.style.display = "none";
+                    //platformer.style.display = "none";
                     // display neither's games, eventually add text that says "No games found."
                   }
                 }
+            if (searchBar.value == 0 && searchItem[0] !== "0") {
+              game.style.display = "block";
+              console.log("yay");
+            } 
         }
+
         return count;
       }
       const myString = searchItem;    
       const occurrences = countLetters(myString, gameDevMap);
       console.log(occurrences); // Output: 8
-      if (searchBar.value == 0 && searchItem[0] !== "0") {
-        // The search input is empty
-        automaton.style.display = "block";
-        platformer.style.display = "block";
-    }
     //////////////////////////////////////////////////////////////
 })
