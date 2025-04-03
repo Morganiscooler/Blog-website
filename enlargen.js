59f45851cfc5ad1cc5dc36a439a7939d811d3581
Morgan C <morgancuiuwu@gmail.com>
2025-04-03 16:43:50 -0400

    games link to their respective pages, need to organize games in folder later

--- enlargen.js
+++ enlargen.js
@@ -1,7 +1,53 @@
-let game = document.getElementById("game");
+var elem = document.getElementById("automatonGame");
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+var elem2 = document.getElementById("friendlyForest");
+function openFullscreen2() {
+  if (elem2.requestFullscreen) {
+    elem2.requestFullscreen();
+  } else if (elem2.webkitRequestFullscreen) { /* Safari */
+    elem2.webkitRequestFullscreen();
+  } else if (elem2.msRequestFullscreen) { /* IE11 */
+    elem2.msRequestFullscreen();
+  }
+}
 
-function enlargen(){
-    game.style.width = "100%";
-    game.style.height = "100%";
+var elem3 = document.getElementById("glitchRunner");
+function openFullscreen3() {
+  if (elem3.requestFullscreen) {
+    elem3.requestFullscreen();
+  } else if (elem3.webkitRequestFullscreen) { /* Safari */
+    elem3.webkitRequestFullscreen();
+  } else if (elem3.msRequestFullscreen) { /* IE11 */
+    elem3.msRequestFullscreen();
+  }
+}
 
+var elem4 = document.getElementById("ticTacToe");
+function openFullscreen4() {
+  if (elem4.requestFullscreen) {
+    elem4.requestFullscreen();
+  } else if (elem4.webkitRequestFullscreen) { /* Safari */
+    elem4.webkitRequestFullscreen();
+  } else if (elem4.msRequestFullscreen) { /* IE11 */
+    elem4.msRequestFullscreen();
+  }
+}
+
+var elem5 = document.getElementById("johnStock");
+function openFullscreen5() {
+  if (elem5.requestFullscreen) {
+    elem5.requestFullscreen();
+  } else if (elem5.webkitRequestFullscreen) { /* Safari */
+    elem5.webkitRequestFullscreen();
+  } else if (elem5.msRequestFullscreen) { /* IE11 */
+    elem5.msRequestFullscreen();
+  }
 }
